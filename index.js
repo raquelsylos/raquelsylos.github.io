35bd703f6fc617bcb8f287e4a91a8dd28437355e
Raquel Sylos <raquelsylos@gmail.com>
2025-01-22 18:55:17 -0300

    fix projects section

--- index.js
+++ index.js
@@ -5,36 +5,28 @@ window.addEventListener('scroll', () => {
   const scrollTop = window.pageYOffset || document.documentElement.scrollTop;
 
   if (scrollTop > lastScrollTop) {
-    // Rolando para baixo
     navbar.classList.remove('nav-down');
     navbar.classList.add('nav-up');
   } else {
-    // Rolando para cima
     navbar.classList.remove('nav-up');
     navbar.classList.add('nav-down');
   }
 
-  lastScrollTop = scrollTop <= 0 ? 0 : scrollTop; // Evita valores negativos
+  lastScrollTop = scrollTop <= 0 ? 0 : scrollTop; // evita valores negativos
 });
 
 function carregarPagina(pagina) {
-  if (pagina === 'projects.html') {
-    // Redireciona para a página completa
-    window.location.href = pagina;
-  } else {
-    // Continua carregando de forma dinâmica para outras páginas
-    fetch(pagina)
-      .then(response => response.text())
-      .then(html => {
-        document.getElementById('content').innerHTML = html;
-      })
-      .catch(error => console.error('Erro ao carregar a página:', error));
-  }
+  // Carregar o conteúdo da página
+  fetch(pagina)
+    .then(response => response.text())
+    .then(html => {
+      // Insere o conteúdo carregado no #content
+      document.getElementById('content').innerHTML = html;
+    })
+    .catch(error => console.error('Erro ao carregar a página:', error));
 }
 
-
-// Se quiser carregar o conteúdo da home automaticamente quando o site abrir
-// Separe o conteúdo da home em um arquivo home-content.html
+// Carregar a home-content.html automaticamente quando o site abrir
 document.addEventListener('DOMContentLoaded', function() {
-  carregarPagina('home-content.html');
+  carregarPagina('home-content.html'); // Carregar conteúdo inicial
 });
